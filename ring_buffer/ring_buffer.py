1691fae1e9d42386e6ab609177aaeb51221d2b6e
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-04-24 10:51:38 -0500

    ring_buffer.py: complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,16 +7,37 @@ class RingBuffer:
         self.current = None
         self.storage = DoublyLinkedList()
 
+# Check to see if at capacity
+# if so move to tail
+# set the value to the item being passed in
+# if prev state set current to prev and assign current to tail
+# otherwise if not at capacity add to the head of the list
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            if self.current is None:
+                self.current = self.storage.tail
+
+            self.current.value = item
+
+            if self.current.prev:
+                self.current = self.current.prev
+
+            else:
+                self.current = self.storage.tail
+
+        elif self.capacity > len(self.storage):
+            self.storage.add_to_head(item)
 
     def get(self):
-        # Note:  This is the only [] allowed
         list_buffer_contents = []
+        selected_node = self.storage.tail
 
-        # TODO: Your code here
-
+        while selected_node:
+            if selected_node.value is not None:
+                list_buffer_contents.append(selected_node.value)
+            selected_node = selected_node.prev
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
